1244b90c56522c5c6c74eb71291b2ae8678296c4
zhaolong <zhaolgyu@163.com>
2019-01-17 20:44:16 +0800

    增加ip地址

--- web/src/main/java/mblog/web/controller/site/IndexController.java
+++ web/src/main/java/mblog/web/controller/site/IndexController.java
@@ -26,6 +26,7 @@ import java.net.InetAddress;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import mblog.nblog.utils.ipUtils;
 
 /**
  * @author langhsu
@@ -45,8 +46,13 @@ public class IndexController extends BaseController{
 		model.put("pn", pn);
 		System.out.println("欢迎页！");
 		IP ip = new IP();
-		ip.setIPAddress(getIpAddr(request));
+		ip.setIPAddress(ipUtils.getIpAddr(request));
 		ip.setLoginTime(LocalDateTime.now().toString());
+		try {
+			ip.setIPAddressName(ipUtils.getAddresses("ip="+ipUtils.getIpAddr(request),"utf-8"));
+		}
+		catch (Exception e){}
+
 		System.out.println("ip:" + ip.getIPAddress());
 		System.out.println("时间:" + ip.getLoginTime());
 		int id = ipSerivce.saveIP(ip);
@@ -55,42 +61,42 @@ public class IndexController extends BaseController{
 	}
 
 
-	public static String getIpAddr(HttpServletRequest request) {
-		String ipAddress = null;
-		try {
-			ipAddress = request.getHeader("x-forwarded-for");
-			if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-				ipAddress = request.getHeader("Proxy-Client-IP");
-			}
-			if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-				ipAddress = request.getHeader("WL-Proxy-Client-IP");
-			}
-			if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-				ipAddress = request.getRemoteAddr();
-				if (ipAddress.equals("127.0.0.1")) {
-					// 根据网卡取本机配置的IP
-					InetAddress inet = null;
-					try {
-						inet = InetAddress.getLocalHost();
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					ipAddress = inet.getHostAddress();
-				}
-			}
-			// 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
-			if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
-				// = 15
-				if (ipAddress.indexOf(",") > 0) {
-					ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
-				}
-			}
-		} catch (Exception e) {
-			ipAddress="";
-		}
-		// ipAddress = this.getRequest().getRemoteAddr();
-
-		return ipAddress;
-	}
+//	public static String getIpAddr(HttpServletRequest request) {
+//		String ipAddress = null;
+//		try {
+//			ipAddress = request.getHeader("x-forwarded-for");
+//			if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+//				ipAddress = request.getHeader("Proxy-Client-IP");
+//			}
+//			if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+//				ipAddress = request.getHeader("WL-Proxy-Client-IP");
+//			}
+//			if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+//				ipAddress = request.getRemoteAddr();
+//				if (ipAddress.equals("127.0.0.1")) {
+//					// 根据网卡取本机配置的IP
+//					InetAddress inet = null;
+//					try {
+//						inet = InetAddress.getLocalHost();
+//					} catch (Exception e) {
+//						e.printStackTrace();
+//					}
+//					ipAddress = inet.getHostAddress();
+//				}
+//			}
+//			// 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+//			if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
+//				// = 15
+//				if (ipAddress.indexOf(",") > 0) {
+//					ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+//				}
+//			}
+//		} catch (Exception e) {
+//			ipAddress="";
+//		}
+//		// ipAddress = this.getRequest().getRemoteAddr();
+//
+//		return ipAddress;
+//	}
 
 }
