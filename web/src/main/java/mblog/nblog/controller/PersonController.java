9443fb35ff6bc1343bc01bfd92db61281d3606bb
zhaolong <2844285152@qq.com>
2019-01-23 11:15:05 +0800

    1

--- web/src/main/java/mblog/nblog/controller/PersonController.java
+++ web/src/main/java/mblog/nblog/controller/PersonController.java
@@ -1,10 +1,16 @@
 package mblog.nblog.controller;
 
+import mblog.nblog.entity.IP;
 import mblog.nblog.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import mblog.nblog.mapper.ipMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import mblog.nblog.utils.iputils;
 
 @Controller
 @ResponseBody
@@ -14,10 +20,63 @@ public class PersonController {
     @Autowired
     PersonService personService;
 
+    @Autowired
+    private ipMapper ipMapper;
+
     @ResponseBody
     @RequestMapping("/selectPerson")
     public Object selectPerson(){
         return personService.selectPerson();
+    }
+
+    @ResponseBody
+    @RequestMapping("/searchip")
+    public List<IP> searchip(){
+        List<IP> list = ipMapper.searchIP();
+        List newList = new ArrayList();
+//        for(int i=0;i<list.size();i++){
+//            System.out.println(iputils.getAddresses(list.get(i).getIPAddress().toString(),"utf-8") + ":--" + list.get(i).getLoginTime());
+//        }
+
+
+        int i=0;
+        System.out.println("第" + i++ + "次--:" +  iputils.getAddresses(list.get(i).getIPAddress().toString(),"utf-8") + ":--" + list.get(i).getLoginTime());
+        System.out.println("第" + i++ + "次--:" +  iputils.getAddresses(list.get(i).getIPAddress().toString(),"utf-8") + ":--" + list.get(i).getLoginTime());
+        System.out.println("第" + i++ + "次--:" +  iputils.getAddresses(list.get(i).getIPAddress().toString(),"utf-8") + ":--" + list.get(i).getLoginTime());
+        System.out.println("第" + i++ + "次--:" +  iputils.getAddresses(list.get(i).getIPAddress().toString(),"utf-8") + ":--" + list.get(i).getLoginTime());
+
+//        Runnable r1 = new MyThread();
+//        Thread t1 = new Thread(r1);
+//        t1.setName("1号");
+//        t1.start();
+//
+//
+//        Thread threads[]=new Thread[100];
+//        for(int i=0;i<1000;i++){
+//            threads[i]=new Thread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    System.out.println(Thread.currentThread().getId());
+//                    System.out.println(iputils.getAddresses(list.get((int)Thread.currentThread().getId()).getIPAddress().toString(),"utf-8") + ":--" + list.get((int)Thread.currentThread().getId()).getLoginTime());
+//                }
+//            });
+//                    threads[i].start();
+//        }
+
+        return newList;
+    }
+
+}
+
+class MyThread implements Runnable{
+
+
+    public void run() {
+        //for(int i=0;i<10;i++){
+            System.out.println("----");
+        //}
+
+
     }
 
 }
--- web/src/main/java/mblog/nblog/mapper/ipMapper.java
+++ web/src/main/java/mblog/nblog/mapper/ipMapper.java
@@ -4,9 +4,12 @@ import mblog.nblog.entity.IP;
 import mblog.nblog.entity.Person;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ipMapper {
 
     //IP selectIP(int id);
     int saveIP(IP ip);
+    List<IP> searchIP();
 }
